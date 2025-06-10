40b731c55a2cb2cacaec5091c198af8252edbb43
Gununes <gununes280@gmail.com>
2025-06-09 21:20:46 -0300

    Add arquivos

---
+++ produto/src/main/webapp/confirmar.js
@@ -0,0 +1,11 @@
+/**
+ *  Cornfirmar a exclusão do produto
+ * @param id 
+ */
+
+function confirmar(id) {
+	let resposta = confirm("Confirma a exclusão deste produto?")
+	if(resposta === true) {
+		window.location.href = "delete?id=" + id
+	}
+}
